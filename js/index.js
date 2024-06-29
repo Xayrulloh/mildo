2965eb6421df1fcf59a5bd98d12a5d3008388ffb
khikmatjon <khikmatjon@medio.uz>
2024-06-29 23:43:33 +0500

    refactor: Improve collection section styling and add contact us section

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const elBox = document.querySelector('.site-header')
-const elBtn = document.querySelector('.site-header__button')
-
-elBtn.addEventListener('click', () => {
-    elBox.classList.toggle("site-header--active")
-})
